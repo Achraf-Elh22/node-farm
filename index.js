8d793f24a5464601248f1b3d9c8e83c24bca1a0c
achraf elhamzaoui <achrafelh22@gmail.com>
2020-05-06 16:25:58 +0000

    build the functionality of passing data to placeholders

--- index.js
+++ index.js
@@ -32,19 +32,63 @@ const url = require('url');
 // Server
 /////////////////////////////////////
 
+// TEMPLATES
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template_overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template_card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template_product.html`,
+  'utf-8'
+);
+// DATA
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('Overview Page');
-  } else if (pathName === '/product') {
-    res.end('Product page');
-  } else if (pathName === '/api') {
+  const { pathname, query } = url.parse(req.url, true);
+  // OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+  }
+  // PRODUCT PAGE
+  else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(output);
+  }
+  // API
+  else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
-  } else {
+  }
+  // NOT FOUND (404) PAGE
+  else {
     res.writeHead(404, { 'content-type': 'text/html' });
     res.end('<h1>Page Not Found!!!!</h1>');
   }
